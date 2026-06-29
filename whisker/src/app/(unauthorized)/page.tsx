f80df305d98cee2f5707deaa6cd0e7bc8ed41621
k21hornet <knt0535.tk@gmail.com>
2026-06-29 20:32:11 +0900

    feat: top

--- whisker/src/app/(unauthorized)/page.tsx
+++ whisker/src/app/(unauthorized)/page.tsx
@@ -1,9 +1,20 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/home">子猫こねこね</Link>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-8 py-2">
+      <h1 className="text-4xl font-bold md:text-6xl">
+        子猫を<span className="text-yellow-700">こねこね</span>する
+      </h1>
+      <div className="flex items-center justify-center gap-2">
+        <Button asChild variant="default" className="px-8 py-5 text-xl">
+          <Link href="/auth/login?screen_hint=signup">新規登録</Link>
+        </Button>
+        <Button asChild variant="outline" className="px-8 py-5 text-xl">
+          <Link href="/auth/login">ログイン</Link>
+        </Button>
+      </div>
     </div>
   );
 }
